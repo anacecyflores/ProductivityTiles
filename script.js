31cbf10481ffd71f2b7b5c577298a5581768139a
Cecy Flores <anacecyflores1@gmail.com>
2022-05-11 22:04:22 -0500

    Added Quotes API & added HTML boxes to fit our model

---
+++ script.js
@@ -0,0 +1,13 @@
+// API for Random Quotes- IT WORKS!
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "quotes15.p.rapidapi.com",
+    "X-RapidAPI-Key": "ad93e94650msh239cfa1a954f0bep116be7jsn39bb48d4e937",
+  },
+};
+
+fetch("https://quotes15.p.rapidapi.com/quotes/random/", options)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
