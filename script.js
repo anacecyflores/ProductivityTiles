d95f16bc6e1e46e30f95db3b996858cf82433edf
Cecy Flores <anacecyflores1@gmail.com>
2022-05-14 11:50:51 -0500

    Added Color-coded sections to the different task sections: Green, Yello, Blue and Red

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ fetch("https://quotes15.p.rapidapi.com/quotes/random/", options)
   .then((response) => response.json())
   .then((response) => {
     let quote = document.querySelector("#quote");
-    // let author = document.querySelector("#author");
+    let author = document.querySelector("#author");
     console.log(quote.textContent);
     console.log(response);
     quote.textContent = response.content;
-    // author.textContent
+    author.textContent = response.originator.name;
   });
