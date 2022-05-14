abe846ee8a81bb07e8f5842b484b85aef722bb45
Cecy Flores <anacecyflores1@gmail.com>
2022-05-14 11:40:28 -0500

    Added Calendar input, Quote Api, Image Api

--- script.js
+++ script.js
@@ -9,7 +9,11 @@ const options = {
 
 fetch("https://quotes15.p.rapidapi.com/quotes/random/", options)
   .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
-
-fetch(google)
+  .then((response) => {
+    let quote = document.querySelector("#quote");
+    // let author = document.querySelector("#author");
+    console.log(quote.textContent);
+    console.log(response);
+    quote.textContent = response.content;
+    // author.textContent
+  });
